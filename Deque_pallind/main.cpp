836367d04a824f1be673d2d6594a0c21a621edbb
DJchris97 <christiandekeuka.djitsa@gmail.com>
2022-07-21 10:52:36 -0400

    premiere version du pallindrome utilisant deque STL comme un tableau

--- Deque_pallind/main.cpp
+++ Deque_pallind/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 #include <cctype>
+#include <string>
+#include <deque>
+
+
+
+//function prototype
+bool is_pallindrome ( const std::string &s);
+
 
 int main() {
-	std::cout <<isalpha('S')<< std::endl;
-    std::cout <<isalpha(3)<< std::endl;
-    std::cout <<isalpha(2)<< std::endl;
+    std::cout <<is_pallindrome("quy.12yu0q")<< std::endl;
+    
 	return 0;
+}
+
+
+bool is_pallindrome ( const std::string &s){
+    
+    int n{0};
+    std::deque <char> pall;
+    
+    for (const auto &i: s ){
+        
+        if (isalpha(i) != 0 ){
+            
+            pall.push_back (std::toupper(i));
+        }
+    }
+    
+    if (pall.size() == 1) {
+        return true;
+        
+    }else{
+        int length = pall.size()/2 ;
+    
+    while ( (n < length) & (pall.at(n) == pall.at(pall.size()-n-1)) ){
+        ++n;   
+        } 
+        
+        if(n == length)
+            return true;
+        else
+            return false;
+    }
 }
