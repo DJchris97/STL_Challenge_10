351ba5c4e8f4bea02b32d20bc1a562bedb8a0cff
DJchris97 <christiandekeuka.djitsa@gmail.com>
2022-07-21 11:58:16 -0400

    pallindrome  avec en ajout le rapport des tests du std::vector de string sur le fichier pallin_rep.txt

--- Deque_pallind/main.cpp
+++ Deque_pallind/main.cpp
@@ -4,7 +4,7 @@
 #include <deque>
 #include <vector>
 #include <iomanip>
-
+#include <fstream>
 
 
 //function prototype
@@ -13,18 +13,36 @@ bool is_pallindrome ( const std::string &s);
 
 int main()
 {
+    
+    
     std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
         "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
         "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
    
     std::cout << std::boolalpha;
-    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
-    for(const auto& s : test_strings) {
-        std::cout << std::setw(8) << std::left << is_pallindrome(s)  << s << std::endl;
+    
+    std::fstream out_file {"pallin_rep.txt", std::ios::out};
+    
+    if(!out_file){
+            std::cerr<<"pallin_rep.txt did not open successfully";
+            
+    } else{
+        
+            std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+            out_file<< "Result" <<"   "<< "String" <<"\n";
+    
+                for(const auto& s : test_strings) {
+                    std::cout << std::setw(8) << std::left << is_pallindrome(s)  << s << std::endl;
+                    out_file<< is_pallindrome(s) <<"        "<< s <<"\n";
+                    
+                }
+            std::cout << std::endl;
+        
     }
-    std::cout << std::endl;
+    
     return 0;
 }
+
 
 
 bool is_pallindrome ( const std::string &s){
